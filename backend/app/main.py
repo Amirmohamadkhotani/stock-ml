800bbf03fdb897057271b3f9b1a09dabebe76611
amir <amirmohamad.khotani@gmail.com>
2025-09-13 19:55:34 +0330

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.app.api import symbols, prices, predict
+from app.api import symbols, prices, predict
 
 app = FastAPI()
 
